37cfd03877463e0b579a9e9dc8be4a4c93addece
yuli.dev <yuliponce22233@gmail.com>
2024-07-15 21:55:23 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <img src="https://ayacuchoaldia.com.ar/wp-content/uploads/2024/05/river-123-anos-logo.png" alt="" />
+      <h1>para vos guiye</h1>
     </div>
   )
 }
